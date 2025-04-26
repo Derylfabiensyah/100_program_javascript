dd913ce15510ee6fd043cb93107d308adfa182b5
= <=>
2025-04-26 08:42:33 +0700

    program segi tujuh

---
+++ segitujuh/script.js
@@ -0,0 +1,15 @@
+function hitungSegitujuh() {
+  const sisiInput = document.getElementById('sisi');
+  const kelilingOutput = document.getElementById('keliling');
+
+  const sisi = parseFloat(sisiInput.value);
+
+  if (isNaN(sisi)) {
+      kelilingOutput.textContent = 'Masukkan sisi yang valid.';
+      return;
+  }
+
+  const keliling = 7 * sisi;
+
+  kelilingOutput.textContent = `keliling : ${keliling.toFixed(2)} cm²`;
+}
