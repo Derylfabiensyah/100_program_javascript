1c450ce65f75ba09cab65c5aec27a9d58c1a2ad7
= <=>
2025-04-28 20:40:37 +0700

    program pecahan desimal

---
+++ pecahan_desimal/script.js
@@ -0,0 +1,14 @@
+function desimal() {
+  const pemInput = document.getElementById('pem');
+  const penInput = document.getElementById('pen');
+  const outOutput = document.getElementById('out');
+
+  const penyebut = parseInt(penInput.value);
+  const pembilang = parseInt(pemInput.value);
+
+  if(penyebut == 0){
+    outOutput.textContent = 'Tidak Dapat Membagi Dengan 0'
+  }else{
+    outOutput.textContent = `Hasil Konversi : ${pembilang / penyebut}`
+  }
+}
